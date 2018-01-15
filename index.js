e71f9f3a0a03691300bd7c3c30958c1613f2ab72
Adithi Krishnamoorthy <adithikrishnamoorthy@gmail.com>
2018-01-15 21:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return 
+  return object.assign
 }
 
   describe('updateObjectWithKeyAndValue(object, key, value)', function() {
